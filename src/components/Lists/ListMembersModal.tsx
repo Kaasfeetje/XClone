8fe0741891229fbc4f64abb6eb421a78b2110e0a
Kaasfeetje <max2002online@gmail.com>
2025-03-25 17:44:23 +0100

    typo

--- src/components/Lists/ListMembersModal.tsx
+++ src/components/Lists/ListMembersModal.tsx
@@ -22,7 +22,7 @@ const ListMembersModal = ({ isOpen, setIsOpen, listId }: Props) => {
   useEffect(() => {
     if (inView) {
       if (fetchListMembers.hasNextPage && !fetchListMembers.isLoading) {
-        voidfetchListMembers.fetchNextPage();
+        void fetchListMembers.fetchNextPage();
       }
     }
   }, [
